686d5794966343dab082fcb5aea958ae831b7c07
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 10:27:25 +0600

    added subscribe button

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -6,13 +6,14 @@ const PricingCard = ({data}) => {
     return (
         <div className='bg-amber-600 rounded-xl p-4 space-y-3 text-white'>
             <h1 className='font-bold text-2xl text-white'>{name}</h1>
-            <h4 className='font-semibold text-md text-white'>{price}</h4>
+            <p className='font-semibold text-md text-white'>{price}</p>
             <h1 className='font-semibold text-md text-white'>{description}</h1>
             <div className=' text-white bg-amber-400 border border-white rounded-xl p-3 space-y-3'>
             {
                 features.map((feature, index)=> <Features key={index} feature={feature}/>)
             }
             </div>
+            <button className="btn btn-neutral font-semibold text-md rounded-xl w-full">Subscribe</button>
         </div>
     );
 };
--- src/Components/PricingCards/PricingCards.jsx
+++ src/Components/PricingCards/PricingCards.jsx
@@ -8,7 +8,7 @@ const PricingCards = ({ pricingPromise }) => {
     <>
       <div>
         <h1 className='p-4 text-4xl font-bold'>Get Our Membership</h1>
-        <div className='grid grid-cols-3 gap-4 p-4'>
+        <div className='grid md:grid-cols-3 gap-4 p-4'>
         {
           useData.map(data => (
             <PricingCard key={data.id} data={data} />
