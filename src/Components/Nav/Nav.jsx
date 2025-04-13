d8d9349374d13e8035c172fb2405ffefeb509a79
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 08:46:06 +0600

    added basic open close process

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Menu } from 'lucide-react';
+import React, { useState } from 'react';
 
 // 5 url information, they will have name, path (url path);
 const navigationData = [
@@ -10,15 +11,21 @@ const navigationData = [
   ];
   
 const Nav = () => {
+    const [open, setOpen] = useState(false);
     return (
         <nav className="flex justify-between p-4">
-            <h1 className='font-bold'>PricingCard</h1>
-            <ul className="space-x-6 font-semibold">
+            <div className='flex gap-3' onClick={()=>setOpen(!open)}>
+            {open ? "open" : "close"}
+            <Menu className='mt-1 lg:hidden' >
+            </Menu>
+            <h1 className='font-bold text-xl'>PricingCard</h1>
+            </div>
+            <ul className="space-x-6 font-semibold text-md">
                 {
                     navigationData.map(nav=><a key={nav.id} href={nav.path}>{nav.name}</a>)
                 }
             </ul>
-            <button className='bg-amber-600 font-semibold text-white rounded-lg px-3 py-2 transition hover:bg-green-700'>Sign In</button>
+            <button className='bg-amber-600 text-xl font-semibold text-white rounded-lg md:px-3 md:py-2 p-2 hover:bg-green-700'>Sign In</button>
         </nav>
     );
 };
