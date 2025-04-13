fc4964a19c3ecb608d880dfcfbb0d55ef7a10185
iftakhersifat <hossensifat615@gmail.com>
2025-04-14 00:21:11 +0600

    added data processing

--- src/Components/Axios/Axios.jsx
+++ src/Components/Axios/Axios.jsx
@@ -2,7 +2,24 @@ import React, { use } from 'react';
 
 const Axios = ({axiosPromise }) => {
     const useAxios = use(axiosPromise )
-    console.log (useAxios.data)
+    console.log (useAxios.data);
+    
+    // data processing for the chart
+    const dataProcessing = useAxios.data.map(studentData =>{
+        const mainData ={
+            id: studentData.id,
+            name: studentData.name,
+            math: studentData.math,
+            physics : studentData.physics,
+            chemistry : studentData.chemistry
+        }
+
+        const avg =(studentData.math + studentData.physics + studentData.chemistry)/3;
+        mainData.avg = avg;
+
+        return mainData;
+    })
+    console.log(dataProcessing)
     return (
         <div>
             
