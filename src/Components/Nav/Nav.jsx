13398689034f51375c86d2c835e6416380ca8443
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 08:58:34 +0600

    added navigationData in left in md

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import React, { useState } from 'react';
 
 // 5 url information, they will have name, path (url path);
@@ -12,19 +12,26 @@ const navigationData = [
   
 const Nav = () => {
     const [open, setOpen] = useState(false);
+
+    const links =navigationData.map(nav=><a key={nav.id} href={nav.path}>{nav.name}</a>);
     return (
         <nav className="flex justify-between p-4">
             <div className='flex gap-3' onClick={()=>setOpen(!open)}>
-            {open ? "open" : "close"}
-            <Menu className='mt-1 lg:hidden' >
-            </Menu>
+            {open ? <Menu className='mt-1 lg:hidden' ></Menu>: <X className='mt-1 lg:hidden'></X>}
+            {/* for medium device menu show left side */}
+            <ul className='lg:hidden absolute mt-7 grid grid-cols-1'>
+                {links}
+            </ul>
+            
             <h1 className='font-bold text-xl'>PricingCard</h1>
             </div>
             <ul className="space-x-6 font-semibold text-md">
                 {
-                    navigationData.map(nav=><a key={nav.id} href={nav.path}>{nav.name}</a>)
+                    links
                 }
             </ul>
+
+            
             <button className='bg-amber-600 text-xl font-semibold text-white rounded-lg md:px-3 md:py-2 p-2 hover:bg-green-700'>Sign In</button>
         </nav>
     );
