6aebe675f8f6f1228ca59d77c8b1af9c19011914
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 07:54:42 +0600

    added navbar from daisy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       <h1>pricing-card</h1>
+      <Navbar></Navbar>
     </>
   )
 }
