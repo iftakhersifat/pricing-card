0f040e1a893a77b6583c50aaa4118796c7dbfec1
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 10:17:36 +0600

    added tick icon

--- src/Components/PricingCard/Features.jsx
+++ src/Components/PricingCard/Features.jsx
@@ -1,9 +1,10 @@
+import { CircleCheckBig } from 'lucide-react';
 import React from 'react';
 
 const Features = ({feature}) => {
     return (
-        <div >
-            <p>{feature}</p>
+        <div>
+            <p className='flex gap-4'><CircleCheckBig></CircleCheckBig>{feature}</p>
         </div>
     );
 };
