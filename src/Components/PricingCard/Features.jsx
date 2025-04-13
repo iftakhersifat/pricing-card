63632b4da8d4e30fdfd8c0189dcc783bcad7c9dc
iftakhersifat <hossensifat615@gmail.com>
2025-04-13 10:12:16 +0600

    added features data by maping

---
+++ src/Components/PricingCard/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Features = ({feature}) => {
+    return (
+        <div >
+            <p>{feature}</p>
+        </div>
+    );
+};
+
+export default Features;
--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Features from './features';
 
 const PricingCard = ({data}) => {
     const {name, price, description, features} =data;
     return (
-        <div className='bg-amber-400 rounded-xl p-4'>
-            <h1>{name}</h1>
-            <h1>{price}</h1>
-            <h1>{description}</h1>
-            <h1>{features}</h1>
+        <div className='bg-amber-600 rounded-xl p-4 space-y-3 text-white'>
+            <h1 className='font-bold text-2xl text-white'>{name}</h1>
+            <h4 className='font-semibold text-md text-white'>{price}</h4>
+            <h1 className='font-semibold text-md text-white'>{description}</h1>
+            <div className=' text-white bg-amber-400 border border-white rounded-xl p-3 space-y-3'>
+            {
+                features.map((feature, index)=> <Features key={index} feature={feature}/>)
+            }
+            </div>
         </div>
     );
 };
